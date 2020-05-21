d5b7c3d0073313b3d320b4847aa127d7870a10af
Renato Matos <renatomiguelmatos@gmail.com>
2020-05-21 03:04:59 +0100

    Delete compile.sh

--- jucomp3.0/compile.sh
+++
@@ -1,4 +0,0 @@
-lex jucompiler.l
-bison -y -d -t -v jucompiler.y
-cc -o jucompiler tree.c symbol_table.c semantics.c lex.yy.c y.tab.c
-zip -r jucompiler.zip jucompiler.l jucompiler.y tree.h tree.c symbol_table.h symbol_table.c semantics.c
